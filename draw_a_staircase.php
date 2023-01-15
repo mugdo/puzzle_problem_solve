0588b74c3e8bbf5199fa88241f7a89366d08c322
Shaiful Islam <mug.shaiful97@gmail.com>
2023-01-15 22:45:43 +0600

    Update draw_a_staircase.php

--- draw_a_staircase.php
+++ draw_a_staircase.php
@@ -3,13 +3,10 @@ $n = 5;
 for ($x = 0; $x <= $n; $x++) {
 	for ($y = 0; $y <= $n; $y++) {
   		if($n-$y>$x){
-        	echo " &nbsp";
-        }else{
-        	echo "#";
-		}
-  		
-
+        	     echo " &nbsp";
+        	}else{
+        	     echo "#";
+		}		
   	}
-  echo "<br>";
-  
+  	echo "<br>";
 } 
